a47153a5bed3a9abb9109fe0e473e7c68a400406
Paul <66725843+Paul-H18@users.noreply.github.com>
2023-12-29 19:01:45 +0100

    Initial commit, no functionsInitial commit, no functions

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('imports')
+
+@endsection
+
+@section('title')
+
+    {{ $title }}
+
+@endsection
+
+@section('content')
+
+@endsection
+
+
+@section('scripts')
+
+@endsection
